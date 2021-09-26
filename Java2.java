3ddade882af8659c91043694281c4d8ae4347150
sadamaction <81281142+sadamaction@users.noreply.github.com>
2021-09-26 22:35:04 +0700

    Add files via upload

---
+++ Java2.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Sadam Fadhilah
+ */
+public class Java2 {
+
+    public static void main (String[]args){
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah   ");
+    }
+}
