8124edf276b3cb3fa94b4c6906b68367f3382699
sadamaction <81281142+sadamaction@users.noreply.github.com>
2021-09-26 22:34:39 +0700

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,15 @@
+
+
+/**
+ *
+ * @author Sadam Fadhilah
+ */
+public class Java1 {
+
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("Dan Sangat Menyenangkan");
+    }
+    
+}
